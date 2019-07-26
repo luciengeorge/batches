694faf1816a4a034beb0052fa427bed96f0630c2
Lucien George <luciengeorge95@gmail.com>
2019-07-26 16:39:32 +0100

    reset db

---
+++ 287/livecodes/sql_crud/test.rb
@@ -0,0 +1,14 @@
+require "sqlite3"
+DB = SQLite3::Database.new("tasks.db")
+DB.results_as_hash = true
+require_relative "task"
+
+# READ (One row)
+
+# CREATE
+
+# UPDATE
+
+# READ (All)
+
+# DESTROY
