74c9811f279a01a2cb3d532fc1af15c8dbc104f0
Lucien George <luciengeorge95@gmail.com>
2020-02-03 11:01:03 +0000

    lecture done

--- 360/lectures/activerecord-basics/active-record-boilerplate/db/seeds.rb
+++ 360/lectures/activerecord-basics/active-record-boilerplate/db/seeds.rb
@@ -1,1 +1,15 @@
 # This is where you can create initial data for your app.
+# Restaurant.create(name: 'hoxton100')
+# Restaurant.create(name: 'hoxton100')
+# Restaurant.create(name: 'hoxton100')
+# Restaurant.create(name: 'hoxton100')
+# Restaurant.create(name: 'hoxton100')
+# Restaurant.create(name: 'hoxton100')
+require 'faker'
+
+100.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name ,
+    address: Faker::Address.street_address
+  )
+end
