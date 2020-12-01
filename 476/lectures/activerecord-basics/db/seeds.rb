0e15ea71af005d569864b4fa1a43988e8433db21
Lucien George <luciengeorge95@gmail.com>
2020-12-01 19:20:26 +0000

    recap done

--- 476/lectures/activerecord-basics/db/seeds.rb
+++ 476/lectures/activerecord-basics/db/seeds.rb
@@ -1,1 +1,8 @@
 # This is where you can create initial data for your app.
+require 'faker'
+
+100.times do |i|
+  restaurant = Restaurant.new(name: Faker::Restaurant.name, address: Faker::Address.street_address, rating: rand(0..5))
+  restaurant.save
+  puts "creating restaurant #{restaurant.name}"
+end
