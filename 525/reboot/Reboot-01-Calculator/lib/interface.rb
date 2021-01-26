35124122f552f57155eda4ff0f928bd18ac899af
Lucien George <luciengeorge95@gmail.com>
2021-01-26 23:33:50 +0200

    refacto

--- 525/reboot/Reboot-01-Calculator/lib/interface.rb
+++ 525/reboot/Reboot-01-Calculator/lib/interface.rb
@@ -29,39 +29,27 @@ while start_over == 'y'
 end
 
 ## solution 2
-loop do
-  # 2. Ask user for the first number => gets.chomp
-  puts "Please enter a number"
-  # 3. Get user answer, and save it in a variable
-  first_number = gets.chomp.to_i
-  # 4. Ask user for second number => gets.chomp
-  puts "Please enter a second number"
-  # 5. save it in another variable
-  second_number = gets.chomp.to_i
-  # ask the user for an operator
-  puts "Enter an operator"
-  operator = gets.chomp
-  # if operator is +
-  # perform an addition
-  result = calculate_result(first_number, second_number, operator)
-  if result == nil
-    puts "Please enter one of [+, -, *, /]"
-  else
-    puts result
-  end
-  puts "Would you like to go again? [y/n]"
-  answer = gets.chomp
-  break if answer == 'n'
-end
-
-
-
-
-
-
-
-
-
-
-
-
+# loop do
+#   # 2. Ask user for the first number => gets.chomp
+#   puts "Please enter a number"
+#   # 3. Get user answer, and save it in a variable
+#   first_number = gets.chomp.to_i
+#   # 4. Ask user for second number => gets.chomp
+#   puts "Please enter a second number"
+#   # 5. save it in another variable
+#   second_number = gets.chomp.to_i
+#   # ask the user for an operator
+#   puts "Enter an operator"
+#   operator = gets.chomp
+#   # if operator is +
+#   # perform an addition
+#   result = calculate_result(first_number, second_number, operator)
+#   if result == nil
+#     puts "Please enter one of [+, -, *, /]"
+#   else
+#     puts result
+#   end
+#   puts "Would you like to go again? [y/n]"
+#   answer = gets.chomp
+#   break if answer == 'n'
+# end
