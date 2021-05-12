b2d0729840bfc4e221d37e8234cf488fc9a175d0
Lucien George <luciengeorge95@gmail.com>
2021-05-12 21:51:03 +0200

    lecture setup for javascript plugins

--- 612/lectures/javascript-plugins/src/index.js
+++ 612/lectures/javascript-plugins/src/index.js
@@ -1,1 +1,28 @@
-console.log("Hello from src/index.js!");
+const movieList = document.querySelector('#results');
+
+const fetchMovies = (searchKeyword) => {
+  fetch(`http://www.omdbapi.com/?apikey=adf1f2d7&s=${searchKeyword}`)
+    .then(response => response.json())
+    .then((data) => {
+      movieList.innerHTML = "";
+      data.Search.forEach((movie) => {
+        movieList.insertAdjacentHTML('beforeend', `
+          <li class="list-inline-item">
+            <img src="${movie.Poster}"/>
+            <p>${movie.Title}</p>
+          </li>
+        `);
+      });
+    });
+};
+
+// get the form from the html
+const form = document.querySelector('#search-form');
+// submit event on the form
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const input = document.querySelector('#search-input');
+  const keyword = input.value;
+  fetchMovies(keyword);
+});
+fetchMovies('harry potter');
