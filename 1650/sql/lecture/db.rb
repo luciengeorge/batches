ccc07783dbc60736422d3c9f2f0dbf022d05864c
Lucien George <luciengeorge95@gmail.com>
2024-05-07 11:43:22 +0100

    Refacto

--- 1650/sql/lecture/db.rb
+++ 1650/sql/lecture/db.rb
@@ -1,7 +1,9 @@
 require 'sqlite3'
 DB = SQLite3::Database.new('patients_doctors.sqlite')
-DB.results_as_hash = true
-DB.execute("insert into doctors (first_name, last_name, age, specialty) values ('Dr. John', 'Doe', 45, 'General Practitioner')")
-rows = DB.execute("select * from doctors")
-pp rows
-pp DB.last_insert_row_id
+
+query = <<-SQL
+  SELECT first_name from doctors order by first_name asc
+SQL
+
+rows = DB.execute(query)
+pp rows.map { |row| row[0] }
