20282b99099fd88b6408c1750bb707ed4dd470bc
Lucien George <luciengeorge95@gmail.com>
2024-05-15 10:36:26 +0100

    Templating lecture done

--- 1650/templating/lecture/movie-list-boilerplate/javascript/application.js
+++ 1650/templating/lecture/movie-list-boilerplate/javascript/application.js
@@ -1,1 +1,22 @@
-console.log("hello from application.js")
+import {createApp} from 'vue'
+
+createApp({
+  data() {
+    return {
+      message: 'Hello from vue js',
+      greeting: 'Hello my name is Lucien',
+    }
+  },
+}).mount('#results')
+
+// const results = document.querySelector('#results')
+// const template = document.querySelector('#movieCardTemplate').innerHTML
+// import Mustache from 'mustachejs'
+
+// fetch('https://www.omdbapi.com/?s=harry%20potter&apikey=adf1f2d7')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const movies = {movies: data.Search}
+//     const html = Mustache.render(template, movies)
+//     results.innerHTML = html
+//   })
